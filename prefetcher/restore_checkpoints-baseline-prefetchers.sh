bd1dde2a1734c794f036563a26dd0ebadbc39b55
Hoa Nguyen <hn@hnpl.org>
2025-03-26 15:48:09 -0700

    Fix baseline prefetcher script
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- prefetcher/restore_checkpoints-baseline-prefetchers.sh
+++ prefetcher/restore_checkpoints-baseline-prefetchers.sh
@@ -2,7 +2,7 @@
 
 #graph_names=("amazon" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats")
 small_graph_names=("amazon" "gplus" "higgs" "roadNetCA" "twitch" "youtube" "web_google" "wiki_talk")
-private_cache_prefetchers=("multiv1" "imp" "ampm" "stride")
+private_cache_prefetchers=("imp" "ampm" "stride")
 OUTPUT_FOLDER="/workdir/ARTIFACTS/results_v2/"
 
 for graph_name in "${small_graph_names[@]}"
@@ -10,6 +10,6 @@ do
     for private_cache_prefetcher in "${private_cache_prefetchers[@]}"
     do
         echo "Running $graph_name with $private_cache_prefetcher"
-        /workdir/gem5/build/ARM/gem5.opt -re --outdir=$OUTPUT_FOLDER/bfs-$graph_name-baseline-$private_cache_prefetcher --debug-flags=PickleDevicePrefetcherProgressTracker experiments/prefetcher/test_restore_checkpoint.py --graph_name=$graph_name --enable_pdev=False --prefetch_distance=0 --private_cache_prefetcher=$private_cache_prefetcher
+        /workdir/gem5/build/ARM/gem5.opt -re --outdir=$OUTPUT_FOLDER/bfs-$graph_name-baseline-$private_cache_prefetcher --debug-flags=PickleDevicePrefetcherProgressTracker experiments/prefetcher/test_restore_checkpoint.py --graph_name=$graph_name --enable_pdev=False --prefetch_distance=0 --private_cache_prefetcher=$private_cache_prefetcher &
     done
 done
