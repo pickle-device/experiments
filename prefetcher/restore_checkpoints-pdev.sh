f67175d62a5c91ec39865e66ba288ffb46f6a9ab
Hoa Nguyen <hn@hnpl.org>
2025-03-30 09:33:52 +0000

    Add restore checkpoint pdev shell scripts
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

---
+++ prefetcher/restore_checkpoints-pdev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+graph_names=("amazon" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats")
+OUTPUT_FOLDER="/workdir/ARTIFACTS/results_v2/"
+
+for graph_name in "${graph_names[@]}"
+do
+    echo "Running $graph_name with no prefetchers"
+    /workdir/gem5/build/ARM/gem5.opt -re --outdir=$OUTPUT_FOLDER/bfs-$graph_name-pdev_distance_32_offset_16 --debug-flags=PickleDevicePrefetcherProgressTracker experiments/prefetcher/test_restore_checkpoint.py --graph_name=$graph_name --enable_pdev=True --prefetch_distance=32 --offset_from_pf_hint=16 --private_cache_prefetcher=none &
+done
