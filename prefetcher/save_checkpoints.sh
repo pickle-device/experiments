7048cee6030c292cd2bc88712e6cd2d24d6e715f
Hoa Nguyen <hn@hnpl.org>
2025-10-29 21:21:32 +0000

    Update save checkpoints script
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- prefetcher/save_checkpoints.sh
+++ prefetcher/save_checkpoints.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-applications=("bfs" "pr")
-graph_names=("amazon" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats" "test5" "test10")
+#applications=("bfs" "pr")
+applications=("bfs")
+graph_names=("test5" "test10" "amazon" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats")
 OUTPUT_FOLDER="/workdir/ARTIFACTS/results_v8/"
 
 for application in "${applications[@]}"
@@ -9,7 +10,7 @@ do
     for graph_name in "${graph_names[@]}"
     do
         echo "Running $application-$graph_name"
-        /workdir/gem5/build/ARM/gem5.opt \
+        HOME=/workdir /workdir/gem5/build/ARM/gem5.opt \
             -re \
             --outdir=$OUTPUT_FOLDER/$application-$graph_name-checkpoint \
             --debug-flags=PickleDevicePrefetcherProgressTracker \
