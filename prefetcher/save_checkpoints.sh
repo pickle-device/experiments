34753822a1b01f6f8f23191cbd2febb3fc08df3a
Hoa Nguyen <hn@hnpl.org>
2025-11-03 08:54:29 +0000

    Update the save checkpoint script to add new workloads and new graphs/matrices
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- prefetcher/save_checkpoints.sh
+++ prefetcher/save_checkpoints.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 #applications=("bfs" "pr")
-applications=("bfs")
-graph_names=("test5" "test10" "amazon" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats")
+applications=("bfs" "tc" "cc")
+#graph_names=("test5" "test10" "amazon" "as_skitter" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats")
+graph_names=("test5" "test10" "amazon" "as_skitter" "livejournal" "orkut" "pokec" "roadNetCA" "youtube" "web_berkstan" "web_google" "wiki_talk")
 OUTPUT_FOLDER="/workdir/ARTIFACTS/results_v8/"
 
+for application in "${applications[@]}"
+do
+    for graph_name in "${graph_names[@]}"
+    do
+        echo "Running $application-$graph_name"
+        HOME=/workdir /workdir/gem5/build/ARM/gem5.opt \
+            -re \
+            --outdir=$OUTPUT_FOLDER/$application-$graph_name-checkpoint \
+            --debug-flags=PickleDevicePrefetcherProgressTracker \
+            experiments/prefetcher/gem5_configurations/save_checkpoint.py \
+            --application=$application \
+            --graph_name=$graph_name;
+    done
+done
+
+applications=("spmv")
+graph_names=("steam1" "nlpkkt200" "consph" "roadnet" "Ga41As41H72")
+OUTPUT_FOLDER="/workdir/ARTIFACTS/results_v8/"
 for application in "${applications[@]}"
 do
     for graph_name in "${graph_names[@]}"
