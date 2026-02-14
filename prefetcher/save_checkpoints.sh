a86657517bb4c73658b7dff6982307f91d28cbba
Hoa Nguyen <hn@hnpl.org>
2026-02-14 00:00:42 -0800

    Add mesh
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- prefetcher/save_checkpoints.sh
+++ prefetcher/save_checkpoints.sh
@@ -2,6 +2,7 @@
 
 #applications=("bfs" "pr")
 applications=("bfs" "tc" "cc" "pr")
+meshes=("8" "10")
 #graph_names=("test5" "test10" "amazon" "as_skitter" "gplus" "higgs" "livejournal" "orkut" "pokec" "roadNetCA" "twitch" "youtube" "web_berkstan" "web_google" "wiki_talk" "wiki_topcats")
 graph_names=("test5" "test10" "amazon" "as_skitter" "livejournal" "orkut" "pokec" "roadNetCA" "youtube" "web_berkstan" "web_google" "wiki_talk")
 OUTPUT_FOLDER="/workdir/ARTIFACTS/results_v8/"
@@ -10,15 +11,17 @@ for application in "${applications[@]}"
 do
     for graph_name in "${graph_names[@]}"
     do
-        echo "Running $application-$graph_name"
-        HOME=/workdir /workdir/gem5/build/ARM/gem5.opt \
-            -re \
-            --outdir=$OUTPUT_FOLDER/$application-$graph_name-checkpoint \
-            --debug-flags=PickleDevicePrefetcherProgressTracker \
-            experiments/prefetcher/gem5_configurations/save_checkpoint.py \
-            --application=$application \
-            --graph_name=$graph_name \
-            --single_threaded=False;
+        for mesh in "${meshes[@]}"
+        do
+            echo "Running $application-$graph_name"
+            HOME=/workdir /workdir/gem5/build/ARM/gem5.opt \
+                -re \
+                --outdir=$OUTPUT_FOLDER/$application-$graph_name-checkpoint \
+                experiments/prefetcher/gem5_configurations/save_checkpoint.py \
+                --application=$application \
+                --graph_name=$graph_name \
+                --mesh=$mesh;
+        done
     done
 done
 
@@ -29,14 +32,16 @@ for application in "${applications[@]}"
 do
     for graph_name in "${graph_names[@]}"
     do
-        echo "Running $application-$graph_name"
-        HOME=/workdir /workdir/gem5/build/ARM/gem5.opt \
-            -re \
-            --outdir=$OUTPUT_FOLDER/$application-$graph_name-checkpoint \
-            --debug-flags=PickleDevicePrefetcherProgressTracker \
-            experiments/prefetcher/gem5_configurations/save_checkpoint.py \
-            --application=$application \
-            --graph_name=$graph_name \
-            --single_threaded=False;
+        for mesh in "${meshes[@]}"
+        do
+            echo "Running $application-$graph_name"
+            HOME=/workdir /workdir/gem5/build/ARM/gem5.opt \
+                -re \
+                --outdir=$OUTPUT_FOLDER/$application-$graph_name-checkpoint \
+                experiments/prefetcher/gem5_configurations/save_checkpoint.py \
+                --application=$application \
+                --graph_name=$graph_name \
+                --mesh=$mesh;
+        done
     done
 done
